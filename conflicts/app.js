7e71c2d841340cdd5d5c3b6e9c53db6e48470bec
Wellington Santos <wellington@frota162.com.br>
2025-02-13 22:33:39 -0300

    refact: change function name and variables

--- conflicts/app.js
+++ conflicts/app.js
@@ -1,9 +1,9 @@
-let entrada1 = parseInt(prompt('Digite um número:'));
-let entrada2 = parseFloat(prompt('Digite outro número:'));
+let entradaNum1 = parseFloat(prompt("Digite um número:"));
+let entradaNum2 = parseFloat(prompt("Digite outro número:"));
 
-function calcularNumero(numero1, numero2) {
-    return numero1 + numero2;
-};
+function calcularNumeroSoma(numero1, numero2) {
+  return numero1 + numero2;
+}
 
-let resultado = calcularNumero(entrada1, entrada2);
-console.log(`${entrada1} + ${entrada2} = ${resultado}`);
+let resultado = calcularNumero(entradaNum1, entradaNum2);
+console.log(`${entradaNum1} + ${entradaNum2} = ${resultado}`);
