b6abffbb684f6997ae2b24d65648be0d718efa8c
Moisés <filhomoises1708@gmail.com>
2025-02-14 22:11:39 -0300

    feat: change function to subtraction instead of addition

--- conflicts/app.js
+++ conflicts/app.js
@@ -1,9 +1,9 @@
 let entradaNum1 = parseFloat(prompt("Digite um número:"));
 let entradaNum2 = parseFloat(prompt("Digite outro número:"));
 
-function calcularNumeroSoma(numero1, numero2) {
-  return numero1 + numero2;
+function calcularNumeroSubtrair(numero1, numero2) {
+  return numero1 - numero2;
 }
 
 let resultado = calcularNumero(entradaNum1, entradaNum2);
-console.log(`${entradaNum1} + ${entradaNum2} = ${resultado}`);
+console.log(`${entradaNum1} - ${entradaNum2} = ${resultado}`);
